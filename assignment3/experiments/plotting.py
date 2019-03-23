e81fe582f4dbc5d1bdda688f4940ca5f992609b8
danie lee <danieljoonlee@gmail.com>
2019-03-23 12:24:52 -0700

    update output 2

--- assignment3/experiments/plotting.py
+++ assignment3/experiments/plotting.py
@@ -27,7 +27,7 @@ logger = logging.getLogger(__name__)
 # input_path = 'output.final/'
 # output_path = 'output.final/images/'
 input_path = 'output/'
-output_path = 'output-1-images/'
+output_path = 'output-2-images/'
 to_process = {
     'benchmark': {
         'path': 'benchmark',
