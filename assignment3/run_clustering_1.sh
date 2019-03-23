b3cf1ce0f25d80e54c1336ccb5751702857a6c02
daniel lee <danieljoonlee@gmail.com>
2019-03-23 12:05:52 -0700

    add first dimension outputs

--- assignment3/run_clustering_1.sh
+++ assignment3/run_clustering_1.sh
@@ -4,10 +4,10 @@
 # If you want to first generate data and updated datasets, remove the "--skiprerun" flags below
 
 # spam data set
-python run_experiment.py --ica --dataset1 --dim 60 --skiprerun --verbose --threads -1 > ica-dataset1-clustering.log 2>&1
-python run_experiment.py --pca --dataset1 --dim 4 --skiprerun --verbose --threads -1 > pca-dataset1-clustering.log 2>&1
-python run_experiment.py --rp  --dataset1 --dim 8 --skiprerun --verbose --threads -1 > rp-dataset1-clustering.log  2>&1
-python run_experiment.py --rf  --dataset1 --dim 12 --skiprerun --verbose --threads -1 > rf-dataset1-clustering.log  2>&1
+python3 run_experiment.py --ica --dataset1 --dim 60 --skiprerun --verbose --threads -1 > ica-dataset1-clustering.log 2>&1
+python3 run_experiment.py --pca --dataset1 --dim 4 --skiprerun --verbose --threads -1 > pca-dataset1-clustering.log 2>&1
+# python run_experiment.py --rp  --dataset1 --dim 8 --skiprerun --verbose --threads -1 > rp-dataset1-clustering.log  2>&1
+# python run_experiment.py --rf  --dataset1 --dim 12 --skiprerun --verbose --threads -1 > rf-dataset1-clustering.log  2>&1
 
 
 # iterations
--- assignment3/run_clustering_2.sh
+++ assignment3/run_clustering_2.sh
@@ -6,7 +6,7 @@
 # credit card default dataset
 python3 run_experiment.py --ica --dataset2 --dim 15 --skiprerun --verbose --threads -1 > ica-dataset2-clustering.log 2>&1
 python3 run_experiment.py --pca --dataset2 --dim 3 --skiprerun --verbose --threads -1 > pca-dataset2-clustering.log 2>&1
-# python3 run_experiment.py --rp  --dataset2 --dim 7 --skiprerun --verbose --threads -1 > rp-dataset2-clustering.log  2>&1
+python3 run_experiment.py --rp  --dataset2 --dim 7 --skiprerun --verbose --threads -1 > rp-dataset2-clustering.log  2>&1
 python3 run_experiment.py --rf  --dataset2 --dim 1 --skiprerun --verbose --threads -1 > rf-dataset2-clustering.log  2>&1
 
 # iterations
